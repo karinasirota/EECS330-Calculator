6831fcab542892115964ef6c924338ebe87d855f
karinasirota <karina@u.northwestern.edu>
2016-02-20 16:51:38 -0600

    update

--- calculator.js
+++ calculator.js
@@ -1,3 +1,4 @@
+function main(){
 var display = document.getElementById("display");
 display.innerHTML =3;
 var neg = false;
@@ -7,41 +8,10 @@ var button;
 button = document.getElementById("clear");
 button.onclick = function(e) { display.innerHTML = "0"; dec = false; neg = false; };
 
-button = document.getElementById("button-1");
-button.onclick = function(e) { display.innerHTML += "1"; };
-
-button = document.getElementById("button-2");
-button.onclick = function(e) { display.innerHTML += "2"; };
-
-button = document.getElementById("button-3");
-button.onclick = function(e) { display.innerHTML += "3"; };
-
-button = document.getElementById("button-4");
-button.onclick = function(e) { display.innerHTML += "4"; };
-
-button = document.getElementById("button-5");
-button.onclick = function(e) { display.innerHTML += "5"; };
-
-button = document.getElementById("button-6");
-button.onclick = function(e) { display.innerHTML += "6"; };
-
-button = document.getElementById("button-7");
-button.onclick = function(e) { display.innerHTML += "7"; };
-
-button = document.getElementById("button-8");
-button.onclick = function(e) { display.innerHTML += "8"; };
-
-button = document.getElementById("button-9");
-button.onclick = function(e) { display.innerHTML += "9"; };
-
-button = document.getElementById("button-0");
-button.onclick = function(e) { display.innerHTML += "0"; };
-
-/*Code from Rosalie*/
-/*for (var i = 0; i < 10; i++){
+for (var i = 0; i < 10; i++){
   button = document.getElementById("button-" + i);
   button.onclick = function(event) { display.innerHTML += event.target.id.substring(7); }
-} */
+} 
 
 var button = document.getElementById("button-decimal");
 button.onclick = function(e) { 
@@ -60,4 +30,4 @@ button.onclick = function(e) {
      display.innerHTML = display.innerHTML.substring(1);
      neg =false;}}};
 
-
+}
