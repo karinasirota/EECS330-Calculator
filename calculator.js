7d5329679c276e7ebcab4faac48440ae2b19e58a
karinasirota <karina@u.northwestern.edu>
2016-02-22 18:39:51 -0600

    added some functionality and updated HTML file to show that

--- calculator.js
+++ calculator.js
@@ -1,33 +1,52 @@
-function main(){
-var display = document.getElementById("display");
-display.innerHTML =3;
-var neg = false;
-var dec = false;
-var button;
+$(document).ready(function(){
+  var displays = document.getElementById("display");
+  var neg = false;
+  var dec = false;
+  var button;
 
-button = document.getElementById("clear");
-button.onclick = function(e) { display.innerHTML = "0"; dec = false; neg = false; };
+  button = document.getElementById("clear");
+  button.onclick = function(e) {
+    display.innerHTML = "0";
+    dec = false;
+  };
 
-for (var i = 0; i < 10; i++){
-  button = document.getElementById("button-" + i);
-  button.onclick = function(event) { display.innerHTML += event.target.id.substring(7); }
-} 
-
-var button = document.getElementById("button-decimal");
-button.onclick = function(e) { 
-  if (!dec){
-    display.innerHTML += ".";
-    dec = true;
-  }};
-
-button = document.getElementById("plus-minus");
-button.onclick = function(e) { 
-  if(display.innerHTML != 0){
-    if (neg==false){
-      display.innerHTML = "-" + display.innerHTML;
-    neg = true;}
+for (var i=0; i<10; i++) {
+  var button = document.getElementById("button-" + i);
+  if (button != null) {    
+      button.onclick = function(e) {
+        if (display.innerHTML !=0){
+      display.innerHTML += e.target.id.substring(7);
+    }
     else {
-     display.innerHTML = display.innerHTML.substring(1);
-     neg =false;}}};
+      
+      display.innerHTML = e.target.id.substring(7);
+    }
 
-}
+  }
+}}
+
+
+
+
+  var button = document.getElementById("button-decimal");
+  button.onclick = function(e) {
+    if (!dec) {
+      display.innerHTML += ".";
+      dec = true;
+    }
+  };
+
+  button = document.getElementById("plus-minus");
+  button.onclick = function(e) {
+    if (display.innerHTML != 0) {
+      if (neg == false) {
+        display.innerHTML = "-" + display.innerHTML;
+        neg = true;
+      } else {
+        display.innerHTML = display.innerHTML.substring(1);
+        neg = false;
+      }
+    }
+  };
+
+});
